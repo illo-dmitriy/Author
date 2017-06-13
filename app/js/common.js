c23a42830e6892197425ae8fd1514f9c28b5b56b
Dmitriy Illo <illo.dmitriy@gmail.com>
2017-06-14 00:11:38 +0300

    header and slide

---
+++ app/js/common.js
@@ -0,0 +1,63 @@
+var cbpAnimatedHeader = (function() {
+
+	var docElem = document.documentElement,
+		header = document.querySelector( '.cbp-af-header' ),
+		didScroll = false,
+		changeHeaderOn = 300;
+
+	function init() {
+		window.addEventListener( 'scroll', function( event ) {
+			if( !didScroll ) {
+				didScroll = true;
+				setTimeout( scrollPage, 250 );
+			}
+		}, false );
+	}
+
+	function scrollPage() {
+		var sy = scrollY();
+		if ( sy >= changeHeaderOn ) {
+			classie.add( header, 'cbp-af-header-shrink' );
+		}
+		else {
+			classie.remove( header, 'cbp-af-header-shrink' );
+		}
+		didScroll = false;
+	}
+
+	function scrollY() {
+		return window.pageYOffset || docElem.scrollTop;
+	}
+
+	init();
+
+})();
+
+$(document).ready(function(){
+
+	$("#owl-demo").owlCarousel({
+
+		navigation : false, // Show next and prev buttons
+		slideSpeed : 300,
+		paginationSpeed : 400,
+		singleItem: true,
+		pagination:	false,
+
+		// "singleItem:true" is a shortcut for:
+		// items : 1, 
+		// itemsDesktop : false,
+		// itemsDesktopSmall : false,
+		// itemsTablet: false,
+		// itemsMobile : false
+	});
+
+	var owl = $(".owl-carousel");
+
+	$(".prev").click(function(){
+		owl.trigger("owl.prev");
+	});
+	$(".next").click(function(){
+		owl.trigger("owl.next");
+	});
+
+});
